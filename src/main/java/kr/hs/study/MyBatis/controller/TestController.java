5f8b2bab5ea36d5e738a4e2f9680c742432c70fe
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-08-08 11:58:14 +0900

    delete

--- src/main/java/kr/hs/study/MyBatis/controller/TestController.java
+++ src/main/java/kr/hs/study/MyBatis/controller/TestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -34,8 +35,8 @@ public class TestController {
     }
 
     @GetMapping("/delete/{id}")
-    public String delete(String id) {
+    public String delete(@PathVariable("id") String id) {
         service.delete(id);
-        return "result";
+        return "redirect:/list";
     }
 }
